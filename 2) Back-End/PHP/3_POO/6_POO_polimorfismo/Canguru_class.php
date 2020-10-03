c79ae0bd02ea68b2477d8f5fc30e8451b92bf7eb
MarceloSMBR <lc.marcelomoreira@outlook.com>
2020-10-03 00:52:37 -0300

    Polimorfismo PHP POO

---
+++ 2) Back-End/PHP/3_POO/6_POO_polimorfismo/Canguru_class.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'Mamifero_class.php';
+
+final class Canguru_class extends Mamifero_class {
+    
+    
+    /*Construct*/
+    function Canguru_class() {
+        
+    }
+    
+    /* Métodos primários*/
+    function usar_bolsa(){
+        
+    }
+    
+    function locomover(){
+        echo "<p> O canguru passou a se movimentar...de pulo em pulo..</p>";
+    }
+
+}
