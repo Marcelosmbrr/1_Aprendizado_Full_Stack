acaeeef60f34006960988e3ef3e13005e4e18d79
MarceloSMBR <lc.marcelomoreira@outlook.com>
2021-04-23 20:49:59 -0300

    php att

---
+++ 2_Back-End/2_PHP/12_Composer/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd7e9d170353d2278c30660606084fbf04736b238',
+    'name' => 'marcelosmbr/composer_teste',
+  ),
+  'versions' => 
+  array (
+    'cocur/slugify' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '33197ebf3c3e579c0fd5a89012cc06c5de75e0f6',
+    ),
+    'marcelosmbr/composer_teste' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd7e9d170353d2278c30660606084fbf04736b238',
+    ),
+  ),
+);
---
+++ 2_Back-End/2_PHP/8_DAO/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '167fd3f43585c3e9c08f68ef428e514aa3f4b0cf',
+    'name' => 'marcelosmbr/7_dao',
+  ),
+  'versions' => 
+  array (
+    'marcelosmbr/7_dao' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '167fd3f43585c3e9c08f68ef428e514aa3f4b0cf',
+    ),
+  ),
+);
